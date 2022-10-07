74bde7b6476b402212320cb4166d20af1899c32f
Drake Buentello <dbuentello211607@gmail.com>
2022-10-07 15:29:52 -0500

    done with example React App

--- PART1/part1/src/App.js
+++ PART1/part1/src/App.js
@@ -1,7 +1,23 @@
-const App = () => (
-  <div>
-    <p>Hello World!</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}! You are {props.age} years old!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const age = 12;
+
+  return (
+    <div>
+      <p>The time is {now.toString()}</p>
+      <Hello name = 'George' age = {age + 12} />
+      <Hello name = 'Drake' age = {age + 23} />
+      <Hello name = 'Johnny' age = {age} />
+    </div>
+  )
+}
 
 export default App;
