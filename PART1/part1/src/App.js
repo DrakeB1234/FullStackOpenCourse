f64ba6f29c871aab250704ffc8d3c8c5e029246b
Drake Buentello <dbuentello211607@gmail.com>
2022-10-08 19:55:04 -0500

    did exercises 1.3-1.5

--- PART1/part1/src/App.js
+++ PART1/part1/src/App.js
@@ -1,21 +1,44 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}! You are {props.age} years old!</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const now = new Date()
-  const age = 12;
+  const Header = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+
+  const Content = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+
+  const Total = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+  // making an object
+  const course = {
+    // first attribute
+    name: "Half Stack application development",
+    // second attribute array of object literals
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      },
+    ]
+  }
 
   return (
     <div>
-      <p>The time is {now.toString()}</p>
-      <Hello name = 'George' age = {age + 12} />
-      <Hello name = 'Drake' age = {age + 23} />
-      <Hello name = 'Johnny' age = {age} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
